89a39618144a732a21d959ae2a11895c0acd48e1
Keyvan Ebrahimpour <k1.ebrahimpour@gmail.com>
2021-03-14 12:09:00 +0100

    Add simple implmentation, License, README

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# cpp_cmake_gtest
-c++ template with cmake + gtest
+# crtp all the things
+start to collect knowledge from projects I used CRTP
---
+++ include/SimpleExample.h
@@ -0,0 +1,34 @@
+//
+// Created by keyvan on 3/13/21.
+//
+#ifndef CRTP_SIMPLEEXAMPLE_H
+#define CRTP_SIMPLEEXAMPLE_H
+template <typename D>
+class InterfaceClass{
+public:
+    void doSomething(){
+        this->interface()->doSomewthing();
+    }
+private:
+    // force correct usage by private C'Tor: class Derived1 : public InterfaceClass<Derived1> ...
+    InterfaceClass() {};  // this makes class Derived2 : public InterfaceClass<Derived1> fail to compile BUT
+    friend D;   // then we need Derived class need access to private C'tor
+    D * interface(){
+        static_cast<D*>(this);
+    }
+};
+// Note: Derived class doesn't have a "is a" relation to the base class
+// as one would expect when inheritance is used
+// here the derived class *expands*  the interface by adding more functionality
+// AND it is used directly as oposed to the usual inheritance (including static polymorphism)
+// scheme where the base class exposes the interface and redirects calls to the derived class
+
+class D1: InterfaceClass<D1>{  // Derived class offers the interface of InterfaceClass
+public:
+    void doSomething(){
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+};
+#endif //CRTP_SIMPLEEXAMPLE_H
+
+
